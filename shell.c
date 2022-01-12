0926d2482ba64543faf267e170172c5639d79adf
jtrevino <jmt2235@columbia.edu>
2022-01-12 01:22:45 +0000

    Working on forking/running user input

--- shell.c
+++ shell.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#include <string.h> //strlen()
-#include <stdlib.h> //malloc()
+#include <string.h>  //strlen()
+#include <stdlib.h>  //malloc()
+#include <unistd.h>  //execvp()
+#include <sys/wait.h>//wait()
 
 int printintro(){
 	printf("\n*****************************************\n");
@@ -11,7 +13,8 @@ int printintro(){
 	}
 
 char *getUserInput(){
-	char *buf = (char *)malloc(4096);
+	char *buf = malloc(4096);
+	if(!buf){perror("MALLOC ERROR\n"); exit(0);}
 	//Inside the actual shell loop. From here, we need to
 	//1. Read the command from std in 
 		//DONE
@@ -23,22 +26,29 @@ char *getUserInput(){
 	return buf;
 }
 
-char *getParsedInput(char *tmp){
-	int i = 0; 
-	char *array = (char *)malloc(4096);
+void executeCommand(char **commandArgs){
+	
+	pid_t pid = fork();
 
-//	array[i] = strtok(tmp, " ");
+	if(pid == -1){
+		printf("Fork Failed, enter command again\n");
+		return;
+	}
+	if(pid == 0){
+		//execute command here using execvp or execute file
+		//int execvp(const char *file, char *const argv[]);
+		int returnValue;
+		returnValue = execvp(commandArgs[0], commandArgs);
+		if (returnValue < 0)
+			printf("Error retrieving command...\n");
+		return;
+	}
+	if(pid > 0){
+		wait(NULL);
+		return;
+	}
 
-//	while (array[i] != NULL)
-//		array[++i] = strtok(NULL, " ");
-//
-//	for (int i = 0; i < 5; i++){
-//		printf("{%s}\n", array[i]);
-//	}
-	return array;
 }
-
-
 
 int main(int argc, char *argv[]){
 	
@@ -46,10 +56,29 @@ int main(int argc, char *argv[]){
 	printintro();
 	
 	while(1){
+		//1. Get user Input
 		char *userInput = getUserInput();
 		printf("{%s}\n" , userInput);
-		char *parsedInput = getParsedInput(userInput);
 		
-	}
+		int c;
+		//2. Parsing user input
+		char * token;
+		char * array[4096];
+		memset(array, 0, sizeof(array));
+		int i = 0;
+		array[i] = strtok(userInput, " ");
 
+		while(array[i] != NULL){
+			array[++i] = strtok(NULL, " ");
+		}
+
+		int j = 0;
+		for (j = 0; j < 10; j++){
+			printf("{%s}\n",array[j]);
+		}
+		//3. executing the command
+		executeCommand(array);
+		
+	}//end of while loop
+	return 0;
 }
